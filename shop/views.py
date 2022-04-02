ff86ad5c45d93d89b3f0182801ee15a6e2c02409
ZuckyNeeraj <zuckyneeraj@gmail.com>
2022-04-02 17:45:07 +0530

    removed the nagivation bar and included search functionality and also pagination

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Products
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def index(request):
     products_objects = Products.objects.all()
+
+    product_name = request.GET.get('product_name')
+    if product_name != '' and product_name is not None:
+        products_objects = products_objects.filter(product_name__icontains=product_name)
+
+    paginator = Paginator(products_objects, 3)
+    page = request.GET.get('page')
+    products_objects = paginator.get_page(page)
 
     return render(request, 'index.html', {'products_objects': products_objects})
